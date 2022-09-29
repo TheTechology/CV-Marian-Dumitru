14808c541f5f3c6ede64c5b596d57cefaefc7f4e
Marian DUMITRU <110493446+TheTechology@users.noreply.github.com>
2022-09-29 09:31:45 +0300

    Add files via upload

---
+++ 3/app.js
@@ -0,0 +1,83 @@
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+const img = document.querySelector("#person-img");
+const author = document.querySelector("#author");
+const job = document.querySelector("#job");
+const info = document.querySelector("#info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+let curentIteam = 0;
+
+//ce se afiseze pt prima data pt ca folosesc DOMContentLoaded
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson(curentIteam);
+});
+
+//afisare pers ca functie normala... mai jos ca arrow function
+// function showPerson(){
+
+// }
+
+let showPerson = (curentIteam) => {
+  const item = reviews[curentIteam];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+};
+
+nextBtn.addEventListener("click", () => {
+  curentIteam++;
+  if (curentIteam > reviews.length - 1) {
+    curentIteam = 0;
+  } else {
+    showPerson(curentIteam);
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  curentIteam--;
+  if (curentIteam < 0) {
+    curentIteam = reviews.length - 1;
+  } else {
+    showPerson(curentIteam);
+  }
+});
+
+randomBtn.addEventListener("click", () => {
+  curentIteam = Math.floor(Math.random() * reviews.length);
+  showPerson(curentIteam);
+});
